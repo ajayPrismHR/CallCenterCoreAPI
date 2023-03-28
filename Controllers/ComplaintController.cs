56bfca280d2dbd37e5c5d681b897b92d95547c72
ajayPrismHR <107325460+ajayPrismHR@users.noreply.github.com>
2023-03-28 09:17:52 +0530

    Complaint Save

--- Controllers/ComplaintController.cs
+++ Controllers/ComplaintController.cs
@@ -3,8 +3,8 @@ using CallCenterCoreAPI.Models;
 using CallCenterCoreAPI.Models.QueryModel;
 using CallCenterCoreAPI.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Net;
-
 namespace CallCenterCoreAPI.Controllers
 {
     [ApiController]
@@ -13,18 +13,43 @@ namespace CallCenterCoreAPI.Controllers
     {
         private readonly ILogger _logger;
         private readonly ILoggerFactory _loggerFactory;
-        public ComplaintController(ILogger<ComplaintController> logger, ILoggerFactory loggerFactory)
+        IConfiguration _configuration;
+
+        private string conn = AppSettingsHelper.Setting(Key: "ConnectionStrings:DevConn");
+        public ComplaintController(ILogger<ComplaintController> logger, ILoggerFactory loggerFactory,IConfiguration configuration)
         {
             _logger = logger;
             _loggerFactory = loggerFactory;
+            _configuration = configuration;
         }
 
-        [HttpGet]
+        [HttpPost]
         [Route("SaveComplaint")]
-        public IActionResult SaveComplaint()
+        public IActionResult SaveComplaint(COMPLAINT modelComplaint,int complaintSourceId=1)
         {
-
-            return Ok("Complaint Saved");
+            ILogger<ComplaintRepository> modelLogger = _loggerFactory.CreateLogger<ComplaintRepository>();
+            ComplaintRepository modelComplaintRepository = new ComplaintRepository(modelLogger);
+            int retStatus = 0;
+            string msg = string.Empty;
+            DataSet dsResponse = modelComplaintRepository.SearchComplaint(modelComplaint.KNO);
+            if (dsResponse.Tables[0].Rows.Count > 0)
+            {
+                if (Convert.ToInt16(dsResponse.Tables[0].Rows[0]["COMPLAINT_status"].ToString()) != 2)
+                {
+                    _logger.LogInformation("You Already Have Complaint No." + dsResponse.Tables[0].Rows[0]["COMPLAINT_NO"].ToString() + " with Status " + dsResponse.Tables[0].Rows[0]["cst"]);
+                     return BadRequest("You Already Have Complaint No. " + dsResponse.Tables[0].Rows[0]["COMPLAINT_NO"].ToString() + " with Status " + dsResponse.Tables[0].Rows[0]["cst"]);
+                }
+            }
+            else
+            {
+                retStatus = modelComplaintRepository.SaveComplaint(modelComplaint, complaintSourceId);
+            }
+            if (retStatus == 0)
+                return Ok("Complaint Save Successfully");
+            else
+                return BadRequest("Error in Saving Complaint");
+           
         }
+        
     }
 }
---
+++ Database/Repository/ComplaintRepository.cs
@@ -0,0 +1,102 @@
+﻿using CallCenterCoreAPI.Models.QueryModel;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CallCenterCoreAPI.Database.Repository
+{
+    public class ComplaintRepository
+    {
+        private readonly ILogger<ComplaintRepository> _logger;
+        private string conn = AppSettingsHelper.Setting(Key: "ConnectionStrings:DevConn");
+        public ComplaintRepository(ILogger<ComplaintRepository> logger)
+        {
+            _logger = logger;
+        }
+        public int SaveComplaint(COMPLAINT modelComplaint, int sourceId)
+        {
+            int retStatus = 0;
+            string retMsg = String.Empty; ;
+            COMPLAINT obj = new COMPLAINT();
+            obj = modelComplaint;
+
+            SqlParameter parmretStatus = new SqlParameter();
+            parmretStatus.ParameterName = "@retStatus";
+            parmretStatus.DbType = DbType.Int32;
+            parmretStatus.Size = 8;
+            parmretStatus.Direction = ParameterDirection.Output;
+
+            SqlParameter parmretMsg = new SqlParameter();
+            parmretMsg.ParameterName = "@retMsg";
+            parmretMsg.DbType = DbType.String;
+            parmretMsg.Size = 8;
+            parmretMsg.Direction = ParameterDirection.Output;
+
+
+            SqlParameter parmretComplaint_no = new SqlParameter();
+            parmretComplaint_no.ParameterName = "@retComplaint_no";
+            parmretComplaint_no.DbType = DbType.Int32;
+            parmretComplaint_no.Size = 8;
+            parmretComplaint_no.Direction = ParameterDirection.Output;
+            SqlParameter[] param ={
+                    new SqlParameter("@OFFICE_CODE",modelComplaint.OFFICE_CODE_ID),
+                    new SqlParameter("@CONSUMER_TYPE",1),
+                    new SqlParameter("@COMPLAINT_TYPE",modelComplaint.ComplaintTypeId),
+                    new SqlParameter("@COMPLAINT_SOURCE_ID",sourceId),//modelComplaint.com),
+                    new SqlParameter("@COMPLAINT_CURRENT_STATUS_ID",10),//modelComplaint.COMPLAINT_status_ID),
+                    new SqlParameter("@JE_AREA_ID",modelComplaint.JE_AREA),
+                    new SqlParameter("@COMPLAINT_status",1),//modelComplaint.COMPLAINT_status_ID),
+                    new SqlParameter("@SDO_CODE",modelComplaint.SDO_CODE),
+                    new SqlParameter("@NAME",modelComplaint.NAME),
+
+                    new SqlParameter("@FATHER_NAME",modelComplaint.FATHER_NAME),
+                    new SqlParameter("@KNO",modelComplaint.KNO),
+                    new SqlParameter("@LANDLINE_NO",modelComplaint.LANDLINE_NO),
+                    new SqlParameter("@MOBILE_NO",modelComplaint.MOBILE_NO),
+                    new SqlParameter("@ALTERNATE_MOBILE_NO",modelComplaint.ALTERNATE_MOBILE_NO),
+                    new SqlParameter("@EMAIL",modelComplaint.EMAIL),
+                    new SqlParameter("@ACCOUNT_NO",modelComplaint.ACCOUNT_NO),
+                    new SqlParameter("@ADDRESS1",modelComplaint.ADDRESS1),
+                    new SqlParameter("@ADDRESS2",modelComplaint.ADDRESS2),
+                    new SqlParameter("@ADDRESS3",modelComplaint.ADDRESS3),
+
+                    new SqlParameter("@LANDMARK",modelComplaint.LANDMARK),
+                    new SqlParameter("@CONSUMER_STATUS",modelComplaint.CONSUMER_STATUS),
+                    new SqlParameter("@FEEDER_NAME",modelComplaint.FEEDER_NAME),
+                    new SqlParameter("@AREA_CODE",modelComplaint.AREA_CODE),
+                    new SqlParameter("@SUB_COMPLAINT_TYPE",modelComplaint.SUB_COMPLAINT_TYPE_ID),//modelComplaint.SUB_COMPLAINT_TYPE_ID),
+                    new SqlParameter("@REMARKS",modelComplaint.REMARKS),
+                     new SqlParameter("@USER_ID",modelComplaint.UserId),
+                     new SqlParameter("@AssigneeId",1),
+                    parmretStatus,parmretMsg,parmretComplaint_no};
+            try
+            {
+                SqlHelper.ExecuteNonQuery(conn, CommandType.StoredProcedure, "COMPLAINTS_REGISTER_API", param);
+
+                if (param[37].Value != DBNull.Value)// status
+                    retStatus = Convert.ToInt32(param[37].Value);
+                else
+                    retStatus = 0;
+            }
+            catch (Exception ex)
+            {
+                retStatus = -1;
+            }
+            return retStatus;
+
+        }
+        public DataSet SearchComplaint(string kno)
+        {
+            DataSet ds= new DataSet();
+            try
+            {
+                SqlParameter[] param ={new SqlParameter("@kno",kno)};
+                ds = SqlHelper.ExecuteDataset(conn, CommandType.StoredProcedure, "Search_Complaint", param);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return ds;
+        }
+    }
+}
